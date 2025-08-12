b324f17a9da8680be0e9233790a96f39f831dc0c
Matus Mastena <Shadiness9530@proton.me>
2025-08-12 07:25:42 +0200

    Some CI bullshit

--- mtos-builder.sh
+++ mtos-builder.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # shellcheck disable=SC2086,SC2046
- set -eux -o pipefail
+set -eux -o pipefail
 # shellcheck source=settings
 source settings
 pre_setup() {
-    mkdir -p $proj_dir/cfg
-    echo -n "
+	mkdir -p $proj_dir/cfg
+	echo -n "
         set(CMAKE_SYSTEM_NAME Linux)
         set(CMAKE_SYSROOT \"$1\")
         set(CMAKE_C_COMPILER_TARGET $TARGET)
@@ -18,37 +18,37 @@ pre_setup() {
         set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
         set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
         set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-        set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)" >> $proj_dir/cfg/$TARGET.cmake
+        set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)" >>$proj_dir/cfg/$TARGET.cmake
 }
 llvm_stage1() {
-    pre_setup $1
-    mkdir -p $llvm_build_dir
-    $wget https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-20.1.8.tar.gz -O /tmp/llvm.tar.gz
-    bsdtar -xvf /tmp/llvm.tar.gz -C $llvm_build_dir
-    cd $llvm_build_dir/llvm* || exit
-    cmake -G Ninja \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DLLVM_ENABLE_PROJECTS="lld;clang;libc" \
-      -DLLVM_ENABLE_RUNTIMES="compiler-rt"\
-      -DCMAKE_TOOLCHAIN_FILE=$proj_dir/cfg/$TARGET.cmake \
-      -DLLVM_HOST_TRIPLE=$TARGET \
-      -DCMAKE_INSTALL_PREFIX=$proj_dir/bin \
-      -S llvm \
-      -B build/$TARGET
-    cmake --build build/$TARGET --target=install
+	pre_setup $1
+	mkdir -p $llvm_build_dir
+	$wget https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-20.1.8.tar.gz -O /tmp/llvm.tar.gz
+	bsdtar -xvf /tmp/llvm.tar.gz -C $llvm_build_dir
+	cd $llvm_build_dir/llvm* || exit
+	cmake -G Ninja \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DLLVM_ENABLE_PROJECTS="lld;clang;libc" \
+		-DLLVM_ENABLE_RUNTIMES="compiler-rt" \
+		-DCMAKE_TOOLCHAIN_FILE=$proj_dir/cfg/$TARGET.cmake \
+		-DLLVM_HOST_TRIPLE=$TARGET \
+		-DCMAKE_INSTALL_PREFIX=$proj_dir/bin \
+		-S llvm \
+		-B build/$TARGET
+	cmake --build build/$TARGET --target=install
 }
 musl_stage1() {
-    mkdir -p /tmp/$musl_bdir
-    $wget http://musl.libc.org/releases/musl-1.2.5.tar.gz -O /tmp/musl.tar.gz
-    bsdtar -xvf /tmp/musl.tar.gz -C /tmp/$musl_bdir
-    cd /tmp/$musl_bdir
-    CC="clang -static" $(find . -name configure) --prefix=$1
-    make TARGET=$(arch)-mtos-linux-musl install
+	mkdir -p /tmp/$musl_bdir
+	$wget http://musl.libc.org/releases/musl-1.2.5.tar.gz -O /tmp/musl.tar.gz
+	bsdtar -xvf /tmp/musl.tar.gz -C /tmp/$musl_bdir
+	cd /tmp/$musl_bdir
+	CC="clang -static" $(find . -name configure) --prefix=$1
+	make TARGET=$(arch)-mtos-linux-musl install
 }
 main() {
-    git submodule init recenv
-    git submodule sync
-    git submodule 
+	git submodule init recenv
+	git submodule sync
+	git submodule
 	$rustup target add $target
 	mkdir -p $out_dir
 	for dir in "${mtos_dirs[@]}"; do
@@ -56,10 +56,10 @@ main() {
 	done
 	for dir in $(seq 0 "${#dirs[@]}"); do
 		if [[ $dir == "${#dirs[@]}" ]]; then
-            break
-        else
-            ln -sf $out_dir/${mtos_dirs[dir]} $out_dir/${dirs[dir]} || break
-        fi
+			break
+		else
+			ln -sf $out_dir/${mtos_dirs[dir]} $out_dir/${dirs[dir]} || break
+		fi
 	done
 	musl_stage1 $out_dir
 	llvm_stage1 $out_dir
@@ -71,10 +71,9 @@ main() {
 	done
 	cp $workdir/bin/busybox.static $out_dir/sbin/busybox
 	$out_dir/bin/busybox --install $out_dir/bin
-    ./recenv/tarball2img.sh $out_dir
+	./recenv/tarball2img.sh $out_dir
 }
 rm -rf /tmp/{musl,llvm}_bd $proj_dir/cfg/$TARGET.cmake /tmp/tmp.*
-main
 # if [[ $(id -u) != 0 ]]; then
 # 	providers=(sudo doas run0)
 # 	for escalation in "${providers[@]}"; do
@@ -86,8 +85,8 @@ main
 # 		fi
 # 	done
 # else
-# 	if [[ -z $(command -v rustup) ]]; then
-# 		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-# 	fi
-# 	main
+if [[ -z $(command -v rustup) ]]; then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
+main
 # fi
