95ec6b3b3179ebce7041913d61a632788775d3d4
Matus Mastena <Shadiness9530@proton.me>
2025-08-24 09:22:53 +0200

    Let's try this the other way

--- mtos-builder.sh
+++ mtos-builder.sh
@@ -6,77 +6,33 @@ source settings
 proj_dir=$(pwd)
 wget=$(command -v wget || command -v wget2)
 USE="X -previewer -webengine "
-cp_dirs=(package.use)
 cp_to_etc=(skel os-release)
-gentoo_cmds=(
-			 "getuto"
-			 "emerge -vuDN --with-bdeps=y --changed-use @world"
-			 "emerge --depclean"
-			 "emerge -v eselect-repository linux-firmware display-manager-init sys-kernel/vanilla-sources"
-			 "eselect kernel set 1"
+alpine_cmds=(
+	"apk update"
+	"apk add linux-firmware"	
 )
 main() {
-	rm -rf /tmp/tmp.* $bdir /tmp/gentoo-snapshot.tar.xz
+	rm -rf /tmp/tmp.* $bdir /tmp/alpine-snapshot.tar.xz
 	mkdir -p $bdir
-	for flag in "${flags[@]}"; do
-		if [[ -n "$(curl https://www.gentoo.org/support/use-flags/ | grep "use-flag" | grep $flag | sort | uniq)" ]]; then
-			USE+="$flag "
-			continue
-		else
-			exit 1
-		fi
-	done
-	$wget $mirror/$version -O /tmp/gentoo-snapshot.tar.xz || echo "Snapshot exists, continuing"
-	tar -xpvf /tmp/gentoo-snapshot.tar.xz -C $bdir || true
+	$wget $mirror -O /tmp/alpine-snapshot.tar.xz || echo "Snapshot exists, continuing"
+	tar -xpvf /tmp/alpine-snapshot.tar.xz -C $bdir || true
 	cp /etc/resolv.conf $bdir/etc
-	echo "LLVM_TARGETS=\"X86\"" >> $bdir/etc/portage/make.conf
-	for dir in "${cp_dirs[@]}"; do
-		cp -r $proj_dir/$dir $bdir/etc/portage/$dir
-	done
 	for dir in "${cp_to_etc[@]}"; do
 		cp -r $proj_dir/etc $bdir/etc
 	done
-	for f in $(arch-chroot $bdir find /etc/portage/package.use); do
-		echo u | arch-chroot $bdir dispatch-conf $f
-	done
-	arch-chroot $bdir emerge-webrsync
 	case "$desktop" in
-		gnome | kde)
-			arch-chroot "$bdir" eselect profile set "$(arch-chroot "$bdir" eselect profile list | grep "$desktop" | awk -F'[][]' 'NR==1 {print $2}')"
-			USE+="$desktop "
+		kde)
+			echo plasma | arch-chroot $bdir setup-desktop
 			;;
-		*)
+		gnome | *)
+			echo $desktop | arch-chroot $bdir setup-desktop
 			;;
 	esac
-	for cmd in "${gentoo_cmds[@]}"; do
-		USE=$USE ACCEPT_LICENSE="*" ACCEPT_KEYWORDS="~*" FEATURES="getbinpkg binpkg-request-signature" arch-chroot $bdir $cmd || exit 1
-	done
-	echo "FEATURES='\${FEATURES} getbinpkg binpkg-request-signature'" >> $bdir/etc/portage/make.conf
 	cp -r $proj_dir/etc/* $bdir/etc
 	install -d $bdir/etc/runlevels/$name
-	# Fix some issues with the desktop
-	USE="minimal" ACCEPT_KEYWORDS="~*" FEATURES="getbinpkg binpkg-request-signature" arch-chroot $bdir emerge --oneshot libsndfile
-	# Merge the desktop
-	case "$desktop" in
-		gnome)
-			echo "DISPLAYMANAGER=gdm" | tee $bdir/etc/conf.d/display-manager
-			USE="$USE -qt" ACCEPT_KEYWORDS="~*" FEATURES="getbinpkg binpkg-request-signature" arch-chroot $bdir emerge -v gnome-light gnome-software 
-			;;
-		kde)
-			echo "DISPLAYMANAGER=sddm" | tee $bdir/etc/conf.d/display-manager
-			USE="$USE sddm display-manager -gtk" ACCEPT_KEYWORDS="~*" FEATURES="getbinpkg binpkg-request-signature" arch-chroot $bdir emerge -v kde-plasma/plasma-desktop kde-plasma/powerdevil kde-plasma/systemsettings
-			;;
-		*)
-			;;
-	esac
-	for overlay in "${overlays[@]}"; do
-		arch-chroot $bdir eselect repository enable $overlay || continue
-		arch-chroot $bdir emaint -r $overlay sync || continue
-	done
 	for pkg in "${pkgs[@]}"; do
 		USE=$USE ACCEPT_LICENSE="*" ACCEPT_KEYWORDS="~*" FEATURES="getbinpkg binpkg-request-signature" arch-chroot $bdir emerge -v $pkg 
 	done
-	wait
 	for svc in "${svcs[@]}"; do
 		arch-chroot $bdir rc-update add $svc $name
 	done
