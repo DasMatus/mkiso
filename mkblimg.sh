9a08b8b0b30e63f19f3232022d724635319d343c
Matus Mastena <Shadiness9530@proton.me>
2025-09-09 13:02:34 +0200

    Start working on finishing this

--- mkblimg.sh
+++ mkblimg.sh
@@ -3,24 +3,29 @@
 set -Eeux -o pipefail
 source recovery_settings
 source settings
-img_size=2.2G
 limine_url=https://codeberg.org/Limine/Limine
 limine_version=v9.x-binary
+tabs="\t\t"
 install_limine() {
-    [[ -d /tmp/limine ]]; rm -rf /tmp/limine 
-    $(command -v git) clone $limine_url --branch $limine_version $bdir
-    cp /tmp/limine/BOOT*.efi $1
+    if [[ -d /tmp/limine ]]; then 
+        rm -rf /tmp/limine 
+    fi
+    $(command -v git) clone $limine_url --branch $limine_version /tmp/limine
 }
 main() {
+    install_limine
     mkdir -p $bdir
     bash $(pwd)/mtos-builder.sh
+    img_size=$(( $(du -l recenv/target/system.img | tail --lines 1 | awk '{print $1}') \
+        + $(du -l /tmp/limine | tail --lines 1 | awk '{print $1}') + $(( 1024 * 8 )) ))
     fallocate -l $img_size /tmp/bl_stage0.img
     printf "g\nn\n\n\n\nt\n1\nw\n" | fdisk /tmp/bl_stage0.img
     mkfs.fat -F32 /tmp/bl_stage0.img
     mount /tmp/bl_stage0.img $bdir
     mkdir -p $bdir/EFI/BOOT
-    install_limine $bdir/EFI/BOOT
-    
+    cp /tmp/limine/BOOT*.efi $bdir/EFI/BOOT
+    cp /boot/vmlinuz-$(uname -r) $bdir/kernel
+    echo "/MatuushOS\n$tabs protocol: linux\n$tabs kernel_path: boot():/kernel\n$tabs module_path: boot():/initramfs\n$tabs comment: Boot a better operating system\n//Other operating systems\n/Windows\n$tabs protocol: efi\n$tabs path: boot():/EFI/Microsoft/bootmgfw.efi" >> $bdir/limine.conf 
 }
 if [[ $(id -u) != 0 ]]; then
 	providers=(sudo doas run0)
